b890c109606220c6a79c01ad343eeb4bd69f22ea
KCTHolman <koenholman@gmail.com>
2019-03-14 11:00:08 +0100

    javascript functie werkend voor inladen progressbars

--- optie2.js
+++ optie2.js
@@ -1,92 +1,120 @@
-function objectInViewport1 () {
-  var slideItem = document.querySelector(".container1");
+var container = document.getElementsByClassName('container');
+var checkScroller = false;
+console.log(container);
 
-  var bounding = slideItem.getBoundingClientRect();
+console.log(container[0].getBoundingClientRect().y);
+console.log(container[1].getBoundingClientRect().y);
+console.log(container[2].getBoundingClientRect().y);
+console.log(container[3].getBoundingClientRect().y);
 
-  if (bounding.top >= 100){
-      addClassComeIn1();
+document.addEventListener('click', GiveCurrentY);
+
+function GiveCurrentY() {
+  console.log(window.scrollY);
+}
+console.log(window.scrollY);
+
+function moveLoadingBar(document) {
+  console.log(window.scrollY);
+  if (window.scrollY > container[1].getBoundingClientRect().y && checkScroller == false) {
+    checkScroller = true;
+    console.log('im at that point')
   }
-  console.log(slideItem)
-  
-  function addClassComeIn1() {
-    var element = document.querySelector("hardskill-html-bar");
-    element.classList.add("animation");
-  };
-
-};
-
-//window.onload = function () {
-window.addEventListener('click', buttonChanger);
-window.addEventListener('MSPointerDown', buttonChanger)
-
-var containers = document.getElementsByClassName("containers");//dit zijn de secties
-var counter = 0;// startpunt voor de teller
-var SectionPosition = [];//lege array die we nodig hebben voor het startpunt van iedere sectie
-
-//var addClass = document.querySelector('.button-active');
+}
+document.addEventListener('scroll', moveLoadingBar);
 
 
 
-function buttonChanger() {
 
+// function objectInViewport1 () {
+//   var slideItem = document.querySelector(".container1");
 
-  for (var i = 0; i < containers.length; i++) {
-    console.log(containers[i].getBoundingClientRect());
-    var htmlElement = containers[i].getBoundingClientRect();
-    SectionPosition[i] = htmlElement.top;
-    console.log(SectionPosition[i]);
+//   var bounding = slideItem.getBoundingClientRect();
 
-    console.log(i);
-    //buttons[i].classList.add('button-active');
+//   if (bounding.top >= 100){
+//       addClassComeIn1();
+//   }
+//   console.log(slideItem)
 
-  };
-};
+//   function addClassComeIn1() {
+//     var element = document.querySelector("hardskill-html-bar");
+//     element.classList.add("animation");
+//   };
 
-document.addEventListener("click", sectionScroller);
+// };
 
+// //window.onload = function () {
+// window.addEventListener('click', buttonChanger);
+// window.addEventListener('MSPointerDown', buttonChanger)
 
-function sectionScroller() {
-  var buttons = document.querySelector('.menu-item');//dit zijn de knoppen die gaan veranderen bij iedere sectie
-  counter++;
-  if (counter >= containers.length) {
-    counter = 0;
+// var containers = document.getElementsByClassName("containers");//dit zijn de secties
+// var counter = 0;// startpunt voor de teller
+// var SectionPosition = [];//lege array die we nodig hebben voor het startpunt van iedere sectie
 
-  }
-  console.log(counter);
-  console.log(containers[counter]);
-
-  window.scrollTo({
-    top: SectionPosition[counter],
-    left: 0,
-    behavior: 'smooth'
-
-  });
-
-};
+// //var addClass = document.querySelector('.button-active');
 
 
 
-var grid1 = document.getElementById('content1')
-var gridAll =document.getElementsByClassName('content-grid')
+// function buttonChanger() {
 
-grid1.addEventListener('click', toNone);
 
-function toNone(){
-  grid1.classList.add('content1on');
-  gridAll.classList.add('content-all')
+//   for (var i = 0; i < containers.length; i++) {
+//     console.log(containers[i].getBoundingClientRect());
+//     var htmlElement = containers[i].getBoundingClientRect();
+//     SectionPosition[i] = htmlElement.top;
+//     console.log(SectionPosition[i]);
 
-};
-//wip
-/* var arrow= document.getElementById('fa-arrow-right')
-var button = document.getElementsByName('button')
+//     console.log(i);
+//     //buttons[i].classList.add('button-active');
 
-console.log(arrowdown);
-button.addEventListener('onmouseover', arrowdown());
+//   };
+// };
 
-function arrowdown(){
-  console.log("arrow down functie"+arrowdown);
-  // arrow.classList.remove('fa-arrow-right');
-  // arrow.classList.add('fa-arrow-down');
+// document.addEventListener("click", sectionScroller);
 
-};
- */
+
+// function sectionScroller() {
+//   var buttons = document.querySelector('.menu-item');//dit zijn de knoppen die gaan veranderen bij iedere sectie
+//   counter++;
+//   if (counter >= containers.length) {
+//     counter = 0;
+
+//   }
+//   console.log(counter);
+//   console.log(containers[counter]);
+
+//   window.scrollTo({
+//     top: SectionPosition[counter],
+//     left: 0,
+//     behavior: 'smooth'
+
+//   });
+
+// };
+
+
+
+// var grid1 = document.getElementById('content1')
+// var gridAll =document.getElementsByClassName('content-grid')
+
+// grid1.addEventListener('click', toNone);
+
+// function toNone(){
+//   grid1.classList.add('content1on');
+//   gridAll.classList.add('content-all')
+
+// };
+// //wip
+// /* var arrow= document.getElementById('fa-arrow-right')
+// var button = document.getElementsByName('button')
+
+// console.log(arrowdown);
+// button.addEventListener('onmouseover', arrowdown());
+
+// function arrowdown(){
+//   console.log("arrow down functie"+arrowdown);
+//   // arrow.classList.remove('fa-arrow-right');
+//   // arrow.classList.add('fa-arrow-down');
+
+// };
+//  */
